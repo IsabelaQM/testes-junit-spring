708f2870ab95dd00c81af89173529b724c6bd786
Isabela Queiroz <106333972+IsabelaQM@users.noreply.github.com>
2024-08-04 22:38:37 -0300

    Corrigindo ClientRepository.java

--- src/main/java/com/iftm/client/repositories/ClientRepository.java
+++ src/main/java/com/iftm/client/repositories/ClientRepository.java
@@ -2,6 +2,7 @@ package com.iftm.client.repositories;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
