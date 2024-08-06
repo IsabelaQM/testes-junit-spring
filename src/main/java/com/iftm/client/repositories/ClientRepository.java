196ca8eb1217731f436220ee3d9cdbd544248a62
Isabela Queiroz <106333972+IsabelaQM@users.noreply.github.com>
2024-08-06 16:09:31 -0300

    Update ClientRepository.java

--- src/main/java/com/iftm/client/repositories/ClientRepository.java
+++ src/main/java/com/iftm/client/repositories/ClientRepository.java
@@ -1,6 +1,5 @@
 package com.iftm.client.repositories;
 
-import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,22 +13,38 @@ import com.iftm.client.entities.Client;
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
-        //Fernanda: implementacao do método que busca um cliente pelo nome ignorando maiúsculas e minúsculas
+    // Fernanda: implementacao do método que busca um cliente pelo nome ignorando
+    // maiúsculas e minúsculas
     @Query("SELECT c FROM Client c WHERE LOWER(c.name) = LOWER(:name)")
     Optional<Client> findByNameIgnoreCase(@Param("name") String name);
 
-    //Fernanda: implementacao do método que busca clientes por parte do nome ignorando maiúsculas e minúsculas
+    // Fernanda: implementacao do método que busca clientes por parte do nome
+    // ignorando maiúsculas e minúsculas
     @Query("SELECT c FROM Client c WHERE LOWER(c.name) LIKE LOWER(CONCAT('%', :name, '%'))")
     List<Client> findByNameContainingIgnoreCase(@Param("name") String name);
 
-    // @Query("SELECT c FROM Client c WHERE c.income > :income")
-    // List<Client> findByIncomeGreaterThan(Double income);
+    // Isabela: implementacao do método que busca clientes com salário maior que o
+    // valor informado
+    @Query("SELECT c FROM Client c WHERE c.income > :income")
+    List<Client> findByIncomeGreaterThan(Double income);
 
-    // @Query("SELECT c FROM Client c WHERE c.income < :income")
-    // List<Client> findByIncomeLessThan(Double income);
+    // Isabela: implementacao do método que busca clientes com salário menor que o
+    // valor informado
+    @Query("SELECT c FROM Client c WHERE c.income < :income")
+    List<Client> findByIncomeLessThan(Double income);
 
-    // @Query("SELECT c FROM Client c WHERE c.salary BETWEEN :incomeInit AND :incomeEnd")
-    // List<Client> findByIncomeBetween(Double incomeInit, Double incomeEnd);
+    // Isabela: implementacao do método que busca clientes com salário entre os
+    // valores informados
+    // @Param quando contém múltiplos parâmetros ou quando o nome do parâmetro no
+    // método não corresponde exatamente ao nome do parâmetro na consulta. BETWEEN -
+    // Os limites pertencem ao intervalo
+    @Query("SELECT c FROM Client c WHERE c.income BETWEEN :incomeInit AND :incomeEnd")
+    List<Client> findByIncomeBetween(@Param("incomeInit") Double incomeInit, @Param("incomeEnd") Double incomeEnd);
+
+    // Isabela: implementacao do método que busca clientes com salário igual ao valor fornecido
+    @Query("SELECT c FROM Client c WHERE c.income = :income")
+    List<Client> findByIncomeEqual(@Param("income") Double income);
 
     // List<Client> findByBirthDateBetween(Instant dateInit, Instant dateEnd);
 }
+
